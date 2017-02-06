97a581351b217c2b173766074dcfb9977b713461
marcelooliveira <mclricardo@gmail.com>
2017-02-06 18:52:16 -0200

    Aula 5 - OK

--- TestDrive/TestDrive2.Droid/SQLite_android.cs
+++ TestDrive/TestDrive2.Droid/SQLite_android.cs
@@ -11,13 +11,13 @@ using Android.Views;
 using Android.Widget;
 using SQLite;
 using System.IO;
-//using TestDrive.Data;
+using TestDrive.Data;
 using TestDrive.Droid;
 
 [assembly: Xamarin.Forms.Dependency(typeof(SQLite_android))]
 namespace TestDrive.Droid
 {
-    class SQLite_android /*: ISQLite*/
+    class SQLite_android : ISQLite
     {
         private const string nomeArquivoDB = "Agendamento.db3";
 
