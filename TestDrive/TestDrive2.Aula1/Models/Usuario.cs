ac24d4343543b1cc2732da3055d3efe210d2fecf
marcelooliveira <mclricardo@gmail.com>
2017-02-06 18:12:02 -0200

    Aula 1 - OK
    Aula 1 - OK

---
+++ TestDrive/TestDrive2.Aula1/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrive
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string dataNascimento { get; set; }
+        public string telefone { get; set; }
+        public string email { get; set; }
+    }
+
+    public class LoginResult
+    {
+        public Usuario usuario { get; set; }
+    }
+}
+
---
+++ TestDrive/TestDrive2.Droid/SQLite_android.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using System.IO;
+//using TestDrive.Data;
+using TestDrive.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(SQLite_android))]
+namespace TestDrive.Droid
+{
+    class SQLite_android /*: ISQLite*/
+    {
+        private const string nomeArquivoDB = "Agendamento.db3";
+
+        public SQLiteConnection PegarConexao()
+        {
+            var caminhoDB = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.Path,
+                nomeArquivoDB);
+
+            return new SQLite.SQLiteConnection(caminhoDB);
+        }
+    }
+}
