9bfef3ec31eef4520ecc9e32ecc4781c3e605ee1
marcelooliveira <mclricardo@gmail.com>
2017-02-06 18:42:53 -0200

    Aula 4 - OK

---
+++ TestDrive/TestDrive2.Aula4/Data/AgendamentoDAO.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDrive.Models;
+
+namespace TestDrive.Data
+{
+    class AgendamentoDAO
+    {
+        readonly SQLiteConnection conexao;
+
+        public AgendamentoDAO(SQLiteConnection conexao)
+        {
+            this.conexao = conexao;
+            this.conexao.CreateTable<Agendamento>();
+        }
+
+        public void Salvar(Agendamento agendamento)
+        {
+            conexao.Insert(agendamento);
+        }
+    }
+}
---
+++ TestDrive/TestDrive2.Aula4/Views/MasterView.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDrive.Models;
+using TestDrive.ViewModels;
+using Xamarin.Forms;
+
+namespace TestDrive.Views
+{
+    public partial class MasterView : TabbedPage
+    {
+        public MasterView(Usuario usuario)
+        {
+            InitializeComponent();
+            this.BindingContext = new MasterViewModel(usuario);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            AssinarMensagens();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            CancelarMensagens();
+        }
+
+        private void AssinarMensagens()
+        {
+            MessagingCenter.Subscribe<Usuario>(this, "EditarPerfil",
+                (usuario) =>
+                {
+                    this.CurrentPage = this.Children[1];
+                });
+
+            MessagingCenter.Subscribe<Usuario>(this, "SucessoSalvarUsuario",
+                (usuario) =>
+                {
+                    this.CurrentPage = this.Children[0];
+                });
+        }
+
+        private void CancelarMensagens()
+        {
+            MessagingCenter.Unsubscribe<Usuario>(this, "EditarPerfil");
+            MessagingCenter.Unsubscribe<Usuario>(this, "SucessoSalvarUsuario");
+        }
+    }
+}
