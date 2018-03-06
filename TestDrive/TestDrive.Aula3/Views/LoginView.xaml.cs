be650c617c5c37c113670e928cdff6d804f29204
marcelooliveira <mclricardo@gmail.com>
2018-03-06 15:33:48 -0300

    Aula3

---
+++ TestDrive/TestDrive.Aula3/Views/LoginView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDrive.ViewModels;
+using Xamarin.Forms;
+
+namespace TestDrive.Views
+{
+    public partial class LoginView : ContentPage
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            MessagingCenter.Subscribe<LoginException>(this, "FalhaLogin",
+            async (exc) =>
+            {
+                await DisplayAlert("Login", @"Falha ao efetuar o login. 
+Verifique os dados e tente novamente mais tarde.", "Ok");
+            });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            MessagingCenter.Unsubscribe<LoginException>(this, "FalhaLogin");
+        }
+    }
+}
---
+++ TestDrive/TestDrive.Aula3/Views/MasterView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDrive.Models;
+using TestDrive.ViewModels;
+using Xamarin.Forms;
+
+namespace TestDrive.Views
+{
+    public partial class MasterView : TabbedPage
+    {
+        public MasterViewModel ViewModel { get; private set; }
+        public MasterView(Usuario usuario)
+        {
+            InitializeComponent();
+            this.ViewModel = new MasterViewModel(usuario);
+            this.BindingContext = this.ViewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MessagingCenter.Subscribe<Usuario>(this, "EditarPerfil"
+            , (msg) =>
+            {
+                this.CurrentPage = this.Children[1];
+            });
+
+            MessagingCenter.Subscribe<Usuario>(this, "SucessoSalvarUsuario"
+                , (msg) =>
+                {
+                    this.CurrentPage = this.Children[0];
+                });
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            MessagingCenter.Unsubscribe<Usuario>(this, "EditarPerfil");
+            MessagingCenter.Unsubscribe<Usuario>(this, "SucessoSalvarUsuario");
+        }
+    }
+}
