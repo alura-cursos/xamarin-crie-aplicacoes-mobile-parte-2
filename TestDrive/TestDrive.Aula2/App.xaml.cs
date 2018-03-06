eecad6701a5f0cf49f0f0e6eb28ef21508bb2358
marcelooliveira <mclricardo@gmail.com>
2018-03-06 14:01:05 -0300

    Aula2

---
+++ TestDrive/TestDrive.Aula2/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestDrive.Views;
+using Xamarin.Forms;
+
+namespace TestDrive
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginView();
+        }
+
+        protected override void OnStart()
+        {
+            MessagingCenter.Subscribe<Usuario>(this, "SucessoLogin",
+                (usuario) =>
+                {
+                    MainPage = new NavigationPage(new ListagemView());
+                });
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
